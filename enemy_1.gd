753a8593124c82095cd2f6f6d5e95ecab69c4b5f
Simon_Hallefält <simon@hallefelt.se>
2023-09-21 13:32:24 +0200

    first commit

---
+++ enemy_1.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+#movment
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var direction
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#animation
+@onready var ANI = get_node("AnimationPlayer")
+
+#life
+var dead = false
+@export var hp = 10
+
+
+func _physics_process(delta):
+	#dead
+	if hp <= 0:
+		if ANI.get_current_animation() != "enemy_dead" and !dead:
+			print("dead, " + str(self.get_name()) + " : " + str(ANI.get_current_animation()) + " .")
+			dead = true
+			ANI.set_current_animation("enemy_dead")
+		return
+	
+	#animation
+	ANI.play("enemy_idel")
+	
+	#movment
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	move_and_slide()
+
+
+
+func _on_enemy_1_hitbox_area_entered(area):
+	if area.get_name() == "bullet":
+		print("hit by " + str(area.get_name()))
+		area.queue_free()
+
+func _on_enemy_1_hitbox_area_exited(area):
+	pass
+
+func _on_vision_area_entered(area):
+	if area.get_name() == "player_hitbox":
+		print("can se " + area.get_name())
+
+
+func _on_enemy_1_hitbox_body_entered(body):
+	pass # Replace with function body.
---
+++ player.gd
@@ -0,0 +1,79 @@
+extends CharacterBody2D
+
+#move
+const SPEED = 300.0
+var direction
+@export var JUMP_VELOCITY = -400.0
+@export var spring_SPEED = SPEED * 1.8
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#shoot
+var Bullet = load("res://bulet.tscn")
+var look = 1
+var can_shoot = true
+
+#animation
+@onready var ANI = get_node("AnimationPlayer")
+
+
+func _on_ready():
+	pass
+	#ANI.play("idel")
+
+
+func _physics_process(delta):
+	
+	ANI.play("idel")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta * 2
+
+	# Handle Jump.
+	if Input.is_action_pressed("up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_axis("vänster", "höger")
+	if Input.is_action_pressed("vänster") || Input.is_action_pressed("höger"):
+		if direction != 0:
+			look = direction
+	if direction:
+		if Input.is_action_pressed("spring"):
+			velocity.x = direction * spring_SPEED
+		else:
+			velocity.x = direction * SPEED 
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	move_and_slide()
+	
+	if Input.is_action_pressed("skjut"):
+		shoot(look)
+
+
+func shoot(direction):
+	Bullet = load("res://bulet.tscn")
+	if can_shoot:
+		var b = Bullet.instantiate()
+		owner.add_child(b)
+		var p = position
+		p.y = p.y - 32
+		b.position = p
+		b.direction = direction
+		shoot_timer()
+
+func shoot_timer():
+	can_shoot = false
+	await get_tree().create_timer(0.1).timeout
+	can_shoot = true
+
+
+func _on_player_hitbox_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_player_hitbox_area_exited(area):
+	if area.get_name() == "Level":
+		get_tree().change_scene_to_file("res://nivå1.tscn")
